d17de71cb7c6238058d9b904c81647f3b3ea4c29
Cory Tanner <cory.tanner@dockyard.com>
2019-11-29 11:29:53 +0100

    Fixes #1 - Add app wireframe

---
+++ src/components/footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/components/footer.css';
+
+// Stateless React Component
+const Footer = () => (
+  <footer className="footer">
+    <h2 className="t-body footer__heading">Bitrate Calc is brought to you by</h2>
+    <a href="https://mixer.com/squirt1e" className="t-link footer__link">mixer.com/squirt1e</a>
+  </footer>
+)
+
+export default Footer;
---
+++ src/components/modules/calculator.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../../styles/components/calculator.css';
+
+
+// Stateless React Component
+const Calculator = () => (
+  <section className="calculator">
+    <h3 className="t-section-heading calculator__heading">Enter Your Settings</h3>
+    <p className="t-body calculator__copy">Future home of amazing calculator</p>
+    <p className="t-body calculator__copy"></p>
+  </section>
+)
+
+export default Calculator;
