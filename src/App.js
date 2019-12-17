b3f1bff2f848ecdddbc1880811bbf156743aba0f
Cory Tanner <ctannerweb@gmail.com>
2019-12-16 22:36:17 -0700

    Add react-ga (#5)

--- src/App.js
+++ src/App.js
@@ -3,8 +3,15 @@ import './App.css';
 import Hero from './components/hero';
 import Main from './components/main';
 import Footer from './components/footer';
+import ReactGA from 'react-ga';
+
+function initializeAnalytics() {
+  ReactGA.initialize('UA-154481477-2');
+  ReactGA.pageview('/index');
+}
 
 function App() {
+  initializeAnalytics()
   return (
     <div className="App">
       <Hero />
