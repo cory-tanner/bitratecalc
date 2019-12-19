1e778aec33212d118540575d764a96711218a99a
Cory Tanner <cory.tanner@dockyard.com>
2019-12-19 08:36:21 -0700

    Add more styling

--- src/components/modules/socials.js
+++ src/components/modules/socials.js
@@ -3,16 +3,12 @@ import SVGIcon from "./SVGIcon";
 
 const Socials = () => (
   <nav className="socials">
-    <a className="socials__link" href="https://twitter.com/Ctannerweb" aria-label="Cory Tanner’s Twitter">
+    <a className="socials__link" href="https://twitter.com/squirt11e" aria-label="Cory Tanner’s Twitter">
       <SVGIcon name="twitter" className="socials__social" />
     </a>
 
-    <a className="socials__link" href="https://github.com/Ctannerweb" aria-label="Cory Tanner’s GitHub">
-      <SVGIcon name="github" className="socials__social" />
-    </a>
-
-    <a className="socials__link" href="https://linkedin.com/in/cory-tanner/" aria-label="Cory Tanner’s LinkedIn">
-      <SVGIcon name="linkedin" className="socials__social" />
+    <a className="socials__link" href="https://mixer.com/squirt1e" aria-label="Cory Tanner’s Mixer">
+      <SVGIcon name="mixer" className="socials__social" />
     </a>
   </nav>
 )
