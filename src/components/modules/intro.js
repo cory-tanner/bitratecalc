5ca0f2c48e7b1ddc3346b7c6be5abfa3d73a8dfa
Cory Tanner <ctannerweb@gmail.com>
2019-12-18 22:21:33 -0700

    Add styling basics (#10)

--- src/components/modules/intro.js
+++ src/components/modules/intro.js
@@ -3,7 +3,7 @@ import React from 'react';
 // Stateless React Component
 const Intro = () => (
   <section className="section">
-    <h3 className="t-section-heading section__heading">Bitrate Calculator For Streamers</h3>
+    <h3 className="t-section-heading section__heading">Made For Streamers</h3>
     <p className="t-body section__copy">Hi :)</p>
     <p className="t-body section__copy">Hello again :)</p>
   </section>
