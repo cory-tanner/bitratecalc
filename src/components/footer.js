d6d507e6f0250892c03314a0fb34d181fd80f95a
Cory Tanner <cory.tanner@dockyard.com>
2019-12-21 02:14:10 -0500

    Add color and svg

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 const Footer = () => (
   <footer className="footer">
-    <h2 className="t-body footer__heading">Bitrate Calc is brought to you by</h2>
+    <h2 className="t-footer footer__heading">Bitrate Calc is brought to you by</h2>
     <a href="https://mixer.com/squirt1e" className="t-link footer__link">mixer.com/squirt1e</a>
+    <p className="t-footer footer__copy">All materials © Cory Tanner 2019</p>
   </footer>
 )
 
--- src/components/modules/intro.js
+++ src/components/modules/intro.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 // Stateless React Component
 const Intro = () => (
-  <section className="section">
-    <h3 className="t-section-heading section__heading">Made For Streamers</h3>
-    <p className="t-body section__copy">Hi :)</p>
-    <p className="t-body section__copy">Hello again :)</p>
+  <section className="section grid">
+    <h3 className="t-section-heading grid__item section__heading">Made For Streamers</h3>
+    <p className="t-body grid__item section__copy" data-grid-medium="6">Need an easy calculator for your Mixer/Twitch stream? I got ya <span role="img" aria-label="smile">😁</span>!! The results of <a href="#resultLow" className="t-link">Low</a> and <a href="#resultHigh" className="t-link">High</a> under recommended bitrate are based off industry standards for streaming low movement content like Teamfight Tactics. While the high value will serve you well if you’re playing a game with lots of movement like Modern Warfare and more first person shooters.</p>
+    <p className="t-body grid__item section__copy" data-grid-medium="6">Find your sweet spot for the games you’re playing. The recommended allocation of your personal internets upload speed is 60% dedication for streaming. Feel free to change that value below but be careful. The less amount of Mbps you leave for your game the higher chance you have of getting packet loss and having disruptions in your gameplay.</p>
   </section>
 )
 
