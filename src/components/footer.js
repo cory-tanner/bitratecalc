6cc0815d1db9f89c4d31c87d1d16fc633cc8eff1
Cory Tanner <cory.tanner@dockyard.com>
2020-02-19 20:51:05 -0700

    Optimize Calculator

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Footer = () => (
   <footer className="footer">
     <h2 className="t-footer footer__heading">Bitrate Calc is brought to you by</h2>
-    <a href="https://mixer.com/squirt1e" className="t-link footer__link">mixer.com/squirt1e</a>
-    <p className="t-footer footer__copy">All materials © Cory Tanner 2019</p>
+    <a href="https://github.com/Ctannerweb" className="t-link footer__link">github.com/Ctannerweb</a>
+    <p className="t-footer footer__copy">All materials © Cory Tanner 2020</p>
   </footer>
 )
 
--- src/components/modules/socials.js
+++ src/components/modules/socials.js
@@ -3,12 +3,12 @@ import SVGIcon from "./SVGIcon";
 
 const Socials = () => (
   <nav className="socials">
-    <a className="socials__link" href="https://twitter.com/squirt11e" aria-label="Cory Tanner’s Twitter">
+    <a className="socials__link" href="https://twitter.com/ctannerweb" aria-label="Cory Tanner’s Twitter">
       <SVGIcon name="twitter" className="socials__social" />
     </a>
 
-    <a className="socials__link" href="https://mixer.com/squirt1e" aria-label="Cory Tanner’s Mixer">
-      <SVGIcon name="mixer" className="socials__social" />
+    <a className="socials__link" href="https://github.com/ctannerweb" aria-label="Cory Tanner’s Github">
+      <SVGIcon name="github" className="socials__social" />
     </a>
   </nav>
 )
