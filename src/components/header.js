89b693bf19987b4731ecea50e52b1d2562b93fa4
Cory Tanner <ctannerweb@gmail.com>
2020-03-06 00:55:11 -0700

    Small design changes (#16)

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import Socials from './modules/socials';
+import SVGIcon from './modules/SVGIcon';
 
 const Header = () => (
   <header className="header">
+    <SVGIcon name="logo" className="header__logo" />
     <h1 className="t-display header__display">Bitrate Calc</h1>
     <Socials />
   </header>
