6843b259ae19479a9ea6ecff3baf18bb6ede0592
Cory Tanner <ctannerweb@gmail.com>
2020-07-25 15:06:44 -0600

    Add support section (#21)

--- src/components/main.js
+++ src/components/main.js
@@ -9,6 +9,10 @@ const Main = () => (
       <h3 className="t-section-heading section__heading">Enter Your Streaming Settings</h3>
       <Calculator />
     </section>
+    <section className="section section-support">
+      <h3 className="section__support-heading t-body-heading">Was this useful?</h3>
+      <p className="section__support-copy t-body">Support the developer by using my <a href="https://brave.com/cor272" className="t-link">Brave Referral Link</a> and give the web browser a try!</p>
+    </section>
   </main>
 )
 
