e616720fc492dcf8bc8c8d31ae19652136da78c3
Cory Tanner <ctannerweb@gmail.com>
2021-05-15 15:36:19 -0600

    Update intro copy (#34)

--- src/components/modules/intro.js
+++ src/components/modules/intro.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Intro = () => (
   <section className="section grid">
     <h3 className="t-section-heading grid__item section__heading">Made For Streamers</h3>
-    <p className="t-body grid__item section__copy" data-grid-medium="6">Need an easy calculator for your Mixer/Twitch stream? I got ya <span role="img" aria-label="smile">😁</span>!! The results of <a href="#resultLow" className="t-link">Low</a> under recommended bitrate are based off industry standards for streaming low movement content like Teamfight Tactics. While the <a href="#resultHigh" className="t-link">High</a> value will serve you well if you’re playing a game with lots of movement like Modern Warfare and more first person shooters.</p>
+    <p className="t-body grid__item section__copy" data-grid-medium="6">Need an easy calculator for your Twitch/YouTube stream? I got ya <span role="img" aria-label="smile">😁</span>!! The results of <a href="#resultLow" className="t-link">Low</a> under recommended bitrate are based off industry standards for streaming low movement content like Teamfight Tactics. While the <a href="#resultHigh" className="t-link">High</a> value will serve you well if you’re playing a game with lots of movement like Modern Warfare and more first person shooters.</p>
     <p className="t-body grid__item section__copy" data-grid-medium="6">Find your sweet spot for the games you’re playing. The recommended allocation of your personal internets upload speed is 60% dedication for streaming. Feel free to change that value below but be careful. The less amount of Mbps you leave for your game the higher chance you have of getting packet loss and having disruptions in your gameplay.</p>
   </section>
 )
